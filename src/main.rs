a577975d6f33106c7b757b89e71f2b2656a66a06
naoyukik <mmaga@dstribe.net>
2024-04-10 22:01:44 +0900

    feat: add parse_json, read_json_file function

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
-use serde_json;
 use serde::{Deserialize, Serialize};
-
+use serde_json;
+use serde_json::{Error, Value};
+use std::fs::File;
+use std::io::BufReader;
 
 fn main() {
     println!("hello world");
+    // read json file
+    // let file_path = "message.json";
+    // let json_data = read_json_file(file_path).unwrap();
+    // let message = parse_json(json_data);
+    // parse json
+    // SNS login
+    // send message
 }
 
 pub fn learn_main() -> bool {
     true
 }
 
-// pub fn read_json_file() -> String {
-//     let path = "message.json";
-//     let file = File::open(path).expect("File not found");
-//     let mut reader = BufReader::new(file);
-//     let mut body = String::new();
-//
-//     reader.read_to_string(&mut body).expect("Failed to read file");
-//
-//     body
-// }
+pub fn read_json_file(file_path: &str) -> Result<Value, Error> {
+    let file_path = file_path;
+    let file = File::open(file_path).expect("File not found");
+    let reader = BufReader::new(file);
+
+    let json_object: Value = serde_json::from_reader(reader)?;
+
+    Ok(json_object)
+}
 
 pub fn parse_json(json_string: &str) -> Message {
-    let parsed: Message = serde_json::from_str(&json_string)
-        .expect("Couldn't parse JSON string");
+    let parsed: Message = serde_json::from_str(&json_string).expect("Couldn't parse JSON string");
     parsed
 }
 
@@ -49,26 +56,44 @@ mod tests {
         assert_eq!(true, learn_main());
     }
 
+    #[test]
+    fn learn_value() {
+        let data = read_json_file("./tests/resources/message.json").unwrap();
+        if let Some(sender) = data.get("sender") {
+            println!("Sender -> {}", sender)
+        } else {
+            println!("Sender not found")
+        }
+    }
+
     // test tasks
     // Can read a JSON file and convert it into a Message type
     #[test]
     fn can_parse_json() {
-        let json_string = r#"
-        {
+        let json_string = r#"{
             "content": "Test message",
             "sender": "user1",
             "receivers": ["BlueSky"]
-        }
-        "#;
+        }"#;
         let parsed: Message = parse_json(json_string);
         assert_eq!(parsed.content, "Test message");
         assert_eq!(parsed.sender, "user1");
         assert_eq!(parsed.receivers.len(), 1);
         assert!(parsed.receivers.contains(&Receivers::BlueSky));
     }
-    // fn can_read_json_file() {
-    //     assert_eq!("true", read_json_file())
-    // }
+
+    #[test]
+    fn can_read_json_file() {
+        let left = r#"{
+            "content": "Test message",
+            "sender": "user1",
+            "receivers": ["BlueSky"]
+        }"#;
+        assert_eq!(
+            serde_json::from_str::<Value>(left).unwrap(),
+            read_json_file("./tests/resources/message.json").unwrap()
+        )
+    }
     // fn can_read_convert_message_type_from_json() {}
 
     // Can 'login' to bluesky
