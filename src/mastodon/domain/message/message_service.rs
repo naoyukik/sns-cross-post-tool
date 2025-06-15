1fe9233ec64ebae012673616069e75076aa81006
Kodama Naoyuki <mmaga@dstribe.net>
2025-06-16 08:37:33 +0900

    refactor: Replace MessageTemplate with MergedMessage in service

--- src/mastodon/domain/message/message_service.rs
+++ src/mastodon/domain/message/message_service.rs
@@ -1,7 +1,7 @@
 use crate::mastodon::domain::message::model::commit_message::CommitMessage;
-use crate::shared::domain::message::model::message_template::MessageTemplate;
+use crate::shared::domain::message::model::merged_message::MergedMessage;
 
-pub fn set_post_message(message: &MessageTemplate) -> CommitMessage {
+pub fn set_post_message(message: &MergedMessage) -> CommitMessage {
     let content_with_fixed_hashtags =
         format!("{} {}", message.content, message.fixed_hashtags.mastodon);
     CommitMessage::new(&content_with_fixed_hashtags)
